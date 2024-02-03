d60a9fcbb0b7d18e56aae426d2be964f728beabf
sboris001 <sboris001@gmail.com>
2024-02-03 12:29:19 -0700

    updated(ChessGame) - commented out makeMove function to try and figure out the bug

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -75,16 +75,16 @@ public class ChessGame {
      * @throws InvalidMoveException if move is invalid
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
-        ChessPosition start = move.getStartPosition();
-        ChessPosition end = move.getEndPosition();
-        ChessPiece.PieceType promo = move.getPromotionPiece();
-        ChessPiece piece = this.board.getPiece(start);
-        this.board.addPiece(start, null);
-        if (isNull(promo)){
-            this.board.addPiece(end, piece);
-        } else {
-            this.board.addPiece(end, new ChessPiece(piece.getTeamColor(), promo));
-        }
+//        ChessPosition start = move.getStartPosition();
+//        ChessPosition end = move.getEndPosition();
+//        ChessPiece.PieceType promo = move.getPromotionPiece();
+//        ChessPiece piece = this.board.getPiece(start);
+//        this.board.addPiece(start, null);
+//        if (isNull(promo)){
+//            this.board.addPiece(end, piece);
+//        } else {
+//            this.board.addPiece(end, new ChessPiece(piece.getTeamColor(), promo));
+//        }
 
     }
 
