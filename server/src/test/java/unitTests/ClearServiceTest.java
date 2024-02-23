a60bd94fdcd98e9f1d38068360e2799855e470b9
sboris001 <sboris001@gmail.com>
2024-02-23 12:42:25 -0700

    updated(ClearServiceTests) - passes the test

--- server/src/test/java/unitTests/ClearServiceTest.java
+++
@@ -1,4 +0,0 @@
-package unitTests;
-
-public class ClearServiceTest {
-}
---
+++ server/src/test/java/unitTests/ClearServiceTests.java
@@ -0,0 +1,36 @@
+package unitTests;
+import chess.ChessGame;
+import dataAccess.DataAccessException;
+import dataAccess.MemoryAuthAccess;
+import dataAccess.MemoryGameAccess;
+import dataAccess.MemoryUserAccess;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import service.ClearService;
+
+public class ClearServiceTests {
+    @BeforeAll
+    public static void fillDB() throws DataAccessException {
+        MemoryUserAccess users = new MemoryUserAccess();
+        MemoryGameAccess games = new MemoryGameAccess();
+        MemoryAuthAccess auths = new MemoryAuthAccess();
+
+        users.addUser(new UserData("Spencer", "password", "spencer@email.com"));
+        games.createGame(new GameData(1, "Spencer", "Jaden", "Friendly game", new ChessGame()));
+        auths.createAuth(new AuthData("testToken", "Spencer"));
+    }
+
+    @Test
+    public void simpleAssertionTest() throws DataAccessException {
+        ClearService clear = new ClearService();
+        clear.clearDB();
+
+        Assertions.assertTrue(MemoryUserAccess.users.isEmpty());
+        Assertions.assertTrue(MemoryAuthAccess.auths.isEmpty());
+        Assertions.assertTrue(MemoryGameAccess.games.isEmpty());
+    }
+}
