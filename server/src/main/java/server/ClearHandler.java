dc990d58ab400c3d99d2fc73868884906c220d51
sboris001 <sboris001@gmail.com>
2024-02-23 12:20:20 -0700

    created(ClearServiceTest)

--- server/src/main/java/server/ClearHandler.java
+++ server/src/main/java/server/ClearHandler.java
@@ -1,14 +1,15 @@
 package server;
 
+import service.ClearService;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 public class ClearHandler implements Route {
-
+    ClearService clear = new ClearService();
     @Override
     public Object handle(Request request, Response response) throws Exception {
-
-        return null;
+        clear.clearDB();
+        return "";
     }
 }
