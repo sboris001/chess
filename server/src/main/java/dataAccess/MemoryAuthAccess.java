734cdb2dffc071fef21758d2ee3d6d51c3049363
sboris001 <sboris001@gmail.com>
2024-02-23 10:24:29 -0700

    updated(MemoryAuthAccess)

--- server/src/main/java/dataAccess/MemoryAuthAccess.java
+++ server/src/main/java/dataAccess/MemoryAuthAccess.java
@@ -5,12 +5,11 @@ import model.AuthData;
 import java.util.HashMap;
 
 public class MemoryAuthAccess implements AuthAccess{
-    HashMap<String, String> auths = new HashMap<>();
+    HashMap<String, AuthData> auths = new HashMap<>();
     @Override
     public void createAuth(AuthData auth) throws DataAccessException {
         String username = auth.username();
-        String token = auth.authToken();
-        auths.put(username, token);
+        auths.put(username, auth);
     }
 
     @Override
