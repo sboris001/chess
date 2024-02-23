395bde62af810a962dcc44c72bdb78e5db2061ba
sboris001 <sboris001@gmail.com>
2024-02-23 10:16:23 -0700

    updated(MemoryGameAccess) - updated all methods

--- server/src/main/java/dataAccess/MemoryGameAccess.java
+++ server/src/main/java/dataAccess/MemoryGameAccess.java
@@ -6,29 +6,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MemoryGameAccess implements GameAccess {
-    ArrayList<GameData> games = new ArrayList<>();
+    HashMap<Integer, GameData> games = new HashMap<>();
     @Override
     public void createGame(GameData game) throws DataAccessException {
-        games.add(game);
+        int gameID = game.gameID();
+        games.put(gameID, game);
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
-        return null;
+        return games.get(gameID);
     }
 
     @Override
     public ArrayList<GameData> listGames() throws DataAccessException {
-        return null;
+        return new ArrayList<GameData>(games.values());
     }
 
     @Override
     public void updateGame(int gameID, GameData game) throws DataAccessException {
-
+        if (games.containsKey(gameID)) {
+            games.put(gameID, game);
+        } else {
+            throw new DataAccessException("Game does not exist");
+        }
     }
 
     @Override
     public void clearGames() throws DataAccessException {
-
+        games.clear();
     }
 }
