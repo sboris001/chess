0edfceaed0d53bc993494f442c92febcc823d449
sboris001 <sboris001@gmail.com>
2024-03-04 14:15:12 -0700

    worked on the SQLGameAccess class

--- server/src/main/java/dataAccess/GameAccess.java
+++ server/src/main/java/dataAccess/GameAccess.java
@@ -1,13 +1,14 @@
 package dataAccess;
 
+import exceptions.ResponseException;
 import model.GameData;
 
 import java.util.ArrayList;
 
 public interface GameAccess {
-    public void createGame(GameData game) throws DataAccessException;
-    public GameData getGame(int gameID) throws DataAccessException;
-    public ArrayList<GameData> listGames() throws DataAccessException;
-    public void updateGame(int gameID, GameData game) throws DataAccessException;
-    public void clearGames() throws DataAccessException;
+    public void createGame(GameData game) throws DataAccessException, ResponseException;
+    public GameData getGame(int gameID) throws DataAccessException, ResponseException;
+    public ArrayList<GameData> listGames() throws DataAccessException, ResponseException;
+    public void updateGame(int gameID, GameData game) throws DataAccessException, ResponseException;
+    public void clearGames() throws DataAccessException, ResponseException;
 }
---
+++ server/src/main/java/dataAccess/SQLGameAccess.java
@@ -0,0 +1,138 @@
+package dataAccess;
+
+import com.google.gson.Gson;
+import exceptions.ResponseException;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+public class SQLGameAccess implements GameAccess{
+
+    public SQLGameAccess() throws ResponseException, DataAccessException {
+        configureDatabase();
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  games (
+              gameID int(256) NOT NULL,
+              whiteUsername varchar(256),
+              blackUsername varchar(256),
+              gameName varchar(256),
+              json TEXT DEFAULT NULL,
+              PRIMARY KEY (gameID),
+              INDEX auth_index (gameID)
+            )
+            """
+    };
+
+    private void configureDatabase() throws ResponseException, DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+    private void executeUpdate(String statement, Object... params) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    switch (param) {
+                        case String p -> ps.setString(i + 1, p);
+                        case Integer p -> ps.setInt(i + 1, p);
+                        case UserData p -> ps.setString(i + 1, p.toString());
+                        case null -> ps.setNull(i + 1, NULL);
+                        default -> {
+                        }
+                    }
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    rs.getInt(1);
+                }
+
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    private GameData readGame(ResultSet rs) throws SQLException {
+        var json = rs.getString("json");
+        return new Gson().fromJson(json, GameData.class);
+    }
+
+    @Override
+    public void createGame(GameData game) throws DataAccessException, ResponseException {
+        var statement = "INSERT INTO games (gameID, whiteUsername, blackUsername, gameName, json) VALUES (?, ?, ?, ?, ?)";
+        var json = new Gson().toJson(game);
+        executeUpdate(statement, game.gameID(), game.whiteUsername(), game.blackUsername(), game.gameName(), json);
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException, ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT json FROM games WHERE gameID=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, gameID);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readGame(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<GameData> listGames() throws DataAccessException, ResponseException {
+        ArrayList<GameData> gameList = new ArrayList<>();
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT json FROM games";
+            try (var ps = conn.prepareStatement(statement)) {
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        gameList.add(readGame(rs));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return gameList;
+    }
+
+    @Override
+    public void updateGame(int gameID, GameData game) throws DataAccessException, ResponseException {
+        var statement = "UPDATE games SET gameID=?, whiteUsername=?, blackUsername=?, gameName=?, json=? WHERE gameID=?";
+        String whiteUsername = game.whiteUsername();
+        String blackUsername = game.blackUsername();
+        String gameName = game.gameName();
+        Gson serializer = new Gson();
+        executeUpdate(statement, gameID, whiteUsername, blackUsername, gameName, serializer.toJson(game), gameID);
+    }
+
+    @Override
+    public void clearGames() throws DataAccessException, ResponseException {
+        var statement = "TRUNCATE games";
+        executeUpdate(statement);
+    }
+}
