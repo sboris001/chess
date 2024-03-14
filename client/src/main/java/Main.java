6a114741f502b77d12ed47811731118204842863
sboris001 <sboris001@gmail.com>
2024-03-13 18:01:44 -0600

    Changing the structure of my drawboard method so it can work with any game, not just hard coded

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -6,7 +6,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.print(RESET_BG_COLOR);
         System.out.println("♕ Welcome to the 240 Chess Client: ");
+        ChessBoard startGame = new ChessBoard();
+        startGame.resetBoard();
         var drawBoard = new DrawBoard();
-        drawBoard.drawBoard(args);
+//        drawBoard.drawBoard(startGame, 1);
+//        System.out.print(RESET_BG_COLOR);
+//        System.out.print("\n");
+        drawBoard.drawBoard(startGame, 2);
     }
 }
--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -1,9 +1,15 @@
 package ui;
 
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
+import static java.util.Objects.isNull;
 import static ui.EscapeSequences.*;
 
 public class DrawBoard {
@@ -15,32 +21,46 @@ public class DrawBoard {
     private static final String R = " R "; // Rook
     private static final String Q = " Q "; // Queen
     private static final String K = " K "; // King
-    private static final String[] emptyArray = {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY};
 
 
-    public void drawBoard(String[] args) {
+
+    public void drawBoard(ChessBoard board, int orientation) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-        headerFooter(out, 1);
-        printRow(out, 1, 1,new String[] {R, N, B, K, Q, B, N, R}, "red");
-        printRow(out,1, 2, new String[] {P, P, P, P, P, P, P, P}, "red");
-        printRow(out,1, 3, emptyArray);
-        printRow(out,1, 4, emptyArray);
-        printRow(out,1, 5, emptyArray);
-        printRow(out,1, 6, emptyArray);
-        printRow(out,1, 7, new String[] {P, P, P, P, P, P, P, P}, "blue");
-        printRow(out,1, 8, new String[] {R, N, B, K, Q, B, N, R}, "blue");
-        headerFooter(out, 1);
-        out.println();
-        headerFooter(out, 2);
-        printRow(out,2, 8, new String[] {R, N, B, Q, K, B, N, R}, "blue");
-        printRow(out,2, 7, new String[] {P, P, P, P, P, P, P, P}, "blue");
-        printRow(out,2, 6, emptyArray);
-        printRow(out,2, 5, emptyArray);
-        printRow(out,2, 4, emptyArray);
-        printRow(out,2, 3, emptyArray);
-        printRow(out,2, 2, new String[] {P, P, P, P, P, P, P, P}, "red");
-        printRow(out,2, 1, new String[] {R, N, B, Q, K, B, N, R}, "red");
-        headerFooter(out, 2);
+        headerFooter(out, orientation);
+        String[] rowPiece = new String[8];
+        String[] rowColor = new String[8];
+        for (int i = 1; i < 9; i++) {
+            if (i > 1) {
+                printRow(out, orientation, i-1, rowPiece, rowColor);
+            }
+            for (int j = 1; j < 9; j++) {
+                ChessPosition pos = new ChessPosition(i, j);
+                ChessPiece piece = board.getPiece(pos);
+                if (isNull(piece)) {
+                    rowPiece[j-1] = EMPTY;
+                    rowColor[j-1] = "";
+                } else{
+                    ChessPiece.PieceType type = board.getPiece(pos).getPieceType();
+                    ChessGame.TeamColor color = board.getPiece(pos).getTeamColor();
+                    switch (type){
+                        case null -> rowPiece[j-1] = EMPTY;
+                        case KING -> rowPiece[j-1] = K;
+                        case PAWN -> rowPiece[j-1] = P;
+                        case BISHOP -> rowPiece[j-1] = B;
+                        case ROOK -> rowPiece[j-1] = R;
+                        case KNIGHT -> rowPiece[j-1] = N;
+                        case QUEEN -> rowPiece[j-1] = Q;
+                    }
+                    switch (color) {
+                        case WHITE -> rowColor[j-1] = "blue";
+                        case BLACK -> rowColor[j-1] = "red";
+                        case null -> rowColor[j-1] = "";
+                    }
+                }
+            }
+        }
+        printRow(out, orientation, 8, rowPiece, rowColor);
+        headerFooter(out, orientation);
     }
 
     private static void headerFooter(PrintStream out, int orientation) {
@@ -62,27 +82,31 @@ public class DrawBoard {
 
     }
 
-    private static void printRow(PrintStream out, int orientation, int row, String[] pieces, String ... color) {
+    private static void printRow(PrintStream out, int orientation, int row, String[] pieces, String[] colors) {
         setGrey(out);
         out.printf(" %d ", row);
-        String text = color.length > 0 ? color[0] : "";
-        if (Objects.equals(text, "red")) {
-            out.print(SET_TEXT_COLOR_RED);
-        } else if (Objects.equals(text, "blue")) {
-            out.print(SET_TEXT_COLOR_BLUE);
-        }
-        printList(out, orientation, pieces, row);
+        printList(out, orientation, pieces, row, colors);
         setGrey(out);
         out.printf(" %d ", row);
         out.print(RESET_BG_COLOR);
         out.print("\n");
     }
 
+    private static void checkColor(PrintStream out, int i, String[] colors) {
+        if (Objects.equals(colors[i], "blue")) {
+            out.print(SET_TEXT_COLOR_BLUE);
+        } else if (Objects.equals(colors[i], "red")) {
+            out.print(SET_TEXT_COLOR_RED);
+        } else {
+            out.print(SET_TEXT_COLOR_BLACK);
+        }
+    }
 
-    private static void printList(PrintStream out, int orientation, String[] pieces, int row) {
+    private static void printList(PrintStream out, int orientation, String[] pieces, int row, String[] colors) {
         if (orientation == 1) {
             if (row % 2 != 0) {
                 for (int i = 0; i < pieces.length; i++) {
+                    checkColor(out, i, colors);
                     if (i % 2 != 0) {
                         out.print(SET_BG_COLOR_BLACK);
                         out.print(pieces[i]);
@@ -93,6 +117,7 @@ public class DrawBoard {
                 }
             } else {
                 for (int i = 0; i < pieces.length; i++) {
+                    checkColor(out, i, colors);
                     if (i % 2 != 0) {
                         out.print(SET_BG_COLOR_WHITE);
                         out.print(pieces[i]);
@@ -103,8 +128,9 @@ public class DrawBoard {
                 }
             }
         } else if (orientation == 2) {
-            if (row % 2 == 0) {
+            if (row % 2 != 0) {
                 for (int i = 0; i < pieces.length; i++) {
+                    checkColor(out, i, colors);
                     if (i % 2 != 0) {
                         out.print(SET_BG_COLOR_BLACK);
                         out.print(pieces[i]);
@@ -115,6 +141,7 @@ public class DrawBoard {
                 }
             } else {
                 for (int i = 0; i < pieces.length; i++) {
+                    checkColor(out, i, colors);
                     if (i % 2 != 0) {
                         out.print(SET_BG_COLOR_WHITE);
                         out.print(pieces[i]);
