0864c7524eb956c352775e01f0f99efc0eb1da35
sboris001 <sboris001@gmail.com>
2024-03-18 10:24:04 -0600

    register finished, and login

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,9 +1,13 @@
 package ui;
 
 import com.google.gson.Gson;
+import model.AuthData;
+import model.LoginUser;
 import model.UserData;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.*;
 
@@ -14,7 +18,8 @@ public class ServerFacade {
         serverUrl = url;
     }
 
-    public String registerUser(UserData user) throws IOException {
+    public AuthData registerUser(UserData user) throws IOException {
+        AuthData response;
         try {
             URL url = (new URI(serverUrl + "/user")).toURL();
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
@@ -22,15 +27,43 @@ public class ServerFacade {
             http.setDoOutput(true);
             http.addRequestProperty("Content-Type", "application/json");
             String reqData = new Gson().toJson(user);
-            try (OutputStream reqBody = http.getOutputStream()){
+            try (OutputStream reqBody = http.getOutputStream()) {
                 reqBody.write(reqData.getBytes());
             }
             http.connect();
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                response = new Gson().fromJson(reader, AuthData.class);
+            }
 
         } catch (IOException | URISyntaxException e) {
             throw new IOException("Could not register");
         }
-        return "";
+        return response;
+    }
+
+    public AuthData loginUser(LoginUser user) throws IOException {
+        AuthData response;
+        try {
+            URL url = (new URI(serverUrl + "/session")).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+            String reqData = new Gson().toJson(user);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+            http.connect();
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                response = new Gson().fromJson(reader, AuthData.class);
+            }
+
+        } catch (IOException | URISyntaxException e) {
+            throw new IOException("Could not login");
+        }
+
+        return response;
     }
 
 
--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,7 +1,14 @@
 package clientTests;
 
+import model.AuthData;
+import model.LoginUser;
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
+import ui.ServerFacade;
+
+import java.io.IOException;
+import java.net.*;
 
 
 public class ServerFacadeTests {
@@ -11,19 +18,52 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
     }
 
     @AfterAll
     static void stopServer() {
         server.stop();
+        System.out.println("Server Stopped");
     }
 
+    @BeforeEach
+    public void clearDB() {
+        try{
+            URL url = (new URI("http://localhost:8080" + "/db")).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod("DELETE");
+            http.setDoOutput(true);
+            http.connect();
+            if (http.getResponseCode() == 200) {
+                System.out.println("DB cleared");
+            }
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerTest() {
+        ServerFacade facade = new ServerFacade("http://localhost:" + "8080");
+        try {
+            facade.registerUser(new UserData("fred", "pw", "maile"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void loginTest() {
+        ServerFacade facade = new ServerFacade("http://localhost:" + "8080");
+        try {
+            facade.registerUser(new UserData("fred", "pw", "maile"));
+            AuthData auth = facade.loginUser(new LoginUser("fred", "pw"));
+            System.out.println(auth.authToken());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
