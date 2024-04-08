c3700c48bd9c61a40e3864993d92a115954a5704
sboris001 <sboris001@gmail.com>
2024-04-08 09:59:09 -0600

    Changed the name of a main class in my server module to make it clearer

--- server/src/main/java/StartServer.java
+++ server/src/main/java/StartServer.java
@@ -1,10 +1,8 @@
-import chess.*;
 import dataAccess.DataAccessException;
 import exceptions.ResponseException;
 import server.Server;
-import spark.Spark;
 
-public class Main {
+public class StartServer {
     public static void main(String[] args) throws ResponseException, DataAccessException {
         System.out.println("♕ 240 Chess Server Started Successfully ");
         Server server = new Server();
--- shared/src/main/java/webSocketMessages/userCommands/JoinPlayer.java
+++ shared/src/main/java/webSocketMessages/userCommands/JoinPlayer.java
@@ -2,11 +2,12 @@ package webSocketMessages.userCommands;
 
 import chess.ChessGame;
 import chess.ChessPiece;
+import model.AuthData;
 
 public class JoinPlayer extends UserGameCommand {
     public JoinPlayer(String authToken, Integer GameID, ChessGame.TeamColor color) {
         super(authToken);
         commandType = CommandType.JOIN_PLAYER;
-
     }
+
 }
