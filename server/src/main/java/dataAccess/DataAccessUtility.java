5f18f503981a57f88b2a34ee0001afff1f20bba8
sboris001 <sboris001@gmail.com>
2024-04-10 11:23:24 -0600

    Worked to remove duplicate code in SQLAuthAccess

--- server/src/main/java/dataAccess/DataAccessUtility.java
+++ server/src/main/java/dataAccess/DataAccessUtility.java
@@ -1,7 +1,38 @@
 package dataAccess;
 
-public class DataAccessUtility {
-    public static void executeUpdate(){
+import exceptions.ResponseException;
+import model.UserData;
 
+import java.sql.SQLException;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+public class DataAccessUtility {
+    public static void executeUpdate(String statement, Object... params) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    switch (param) {
+                        case String p -> ps.setString(i + 1, p);
+                        case Integer p -> ps.setInt(i + 1, p);
+                        case UserData p -> ps.setString(i + 1, p.toString());
+                        case null -> ps.setNull(i + 1, NULL);
+                        default -> {
+                        }
+                    }
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    rs.getInt(1);
+                }
+
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
     }
 }
--- server/src/main/java/dataAccess/SQLAuthAccess.java
+++ server/src/main/java/dataAccess/SQLAuthAccess.java
@@ -41,32 +41,6 @@ public class SQLAuthAccess implements AuthAccess{
             throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
         }
     }
-    private void executeUpdate(String statement, Object... params) throws ResponseException {
-        try (var conn = DatabaseManager.getConnection()) {
-            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                    switch (param) {
-                        case String p -> ps.setString(i + 1, p);
-                        case Integer p -> ps.setInt(i + 1, p);
-                        case UserData p -> ps.setString(i + 1, p.toString());
-                        case null -> ps.setNull(i + 1, NULL);
-                        default -> {
-                        }
-                    }
-                }
-                ps.executeUpdate();
-
-                var rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                    rs.getInt(1);
-                }
-
-            }
-        } catch (SQLException | DataAccessException e) {
-            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
-        }
-    }
 
     private AuthData readAuth(ResultSet rs) throws SQLException {
         var json = rs.getString("json");
@@ -77,7 +51,7 @@ public class SQLAuthAccess implements AuthAccess{
     public void createAuth(AuthData auth) throws DataAccessException, ResponseException {
         var statement = "INSERT INTO auths (authToken, username, json) VALUES (?, ?, ?)";
         var json = new Gson().toJson(auth);
-        executeUpdate(statement, auth.authToken(), auth.username(), json);
+        DataAccessUtility.executeUpdate(statement, auth.authToken(), auth.username(), json);
     }
 
     @Override
@@ -101,12 +75,12 @@ public class SQLAuthAccess implements AuthAccess{
     @Override
     public void clearAuths() throws ResponseException {
         var statement = "TRUNCATE auths";
-        executeUpdate(statement);
+        DataAccessUtility.executeUpdate(statement);
     }
 
     @Override
     public void deleteAuth(String authToken) throws ResponseException {
         var statement = "DELETE FROM auths WHERE authToken=?";
-        executeUpdate(statement, authToken);
+        DataAccessUtility.executeUpdate(statement, authToken);
     }
 }
