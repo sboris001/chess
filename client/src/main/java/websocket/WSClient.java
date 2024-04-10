18fc3ba94149f6bdfebe4f35a4cccf44a0549e78
sboris001 <sboris001@gmail.com>
2024-04-10 16:30:14 -0600

    test part 2

---
+++ client/src/main/java/websocket/WSClient.java
@@ -0,0 +1,18 @@
+package websocket;
+import chess.ChessGame;
+import chess.ChessMove;
+import com.google.gson.Gson;
+import model.AuthData;
+import ui.DrawBoard;
+import webSocketMessages.serverMessages.Error;
+import webSocketMessages.serverMessages.LoadGame;
+import webSocketMessages.serverMessages.Notification;
+import webSocketMessages.serverMessages.ServerMessage;
+import webSocketMessages.userCommands.*;
+import javax.websocket.*;
+import java.net.URI;
+
+
+public class WSClient {
+
+}
